458830b7724703a3a33a10d0f4a1e7c18e99e665
Colin Rundel <rundel@gmail.com>
2025-12-01 01:01:02 -0500

    Day 1

--- day01/day01.R
+++ day01/day01.R
@@ -1,2 +1,32 @@
 library(tidyverse)
 
+test = read_lines("day01/test.txt")
+input = read_lines("day01/input.txt")
+
+d = input |> 
+  str_match("([LR])(\\d+)") |>
+  as_tibble() |>
+  set_names(c("command", "dir", "steps")) |>
+  mutate(
+    steps = as.integer(steps),
+    steps = ifelse(dir == "L", -steps, steps)
+  )
+
+## Task 1
+
+c(50, d$steps) |> cumsum() |> (function(x) x %% 100)() |> (function(x) sum(x == 0))()
+
+## Task 2 
+
+x = c(50, d$steps) |> cumsum()
+
+trim = function(x) {
+  x[c(-1, -length(x))]
+}
+
+map2(lag(x)[-1], x[-1], ~ trim(.x:.y) %% 100) |>
+  map_int(~ sum(.x == 0)) |>
+  sum() +
+  c(50, d$steps) |> cumsum() |> (function(x) x %% 100)() |> (function(x) sum(x == 0))()
+
+
